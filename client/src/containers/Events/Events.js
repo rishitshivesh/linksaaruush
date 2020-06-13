f5c7242e63ad05515ef3c279a751c9cea663ea70
Yashvardhan Jagnani <jagnani73@users.noreply.github.com>
2020-06-13 18:24:32 +0530

    sequence changed

--- client/src/containers/Events/Events.js
+++ client/src/containers/Events/Events.js
@@ -19,7 +19,12 @@ class Events extends Component {
   }
 
   componentWillMount() {
-    let token = localStorage.getItem("authToken");
+    let token = null;
+
+    if (window.location.pathname === "/admin/panel") {
+      token = localStorage.getItem("authToken");
+    }
+
     axios
       .get("/api/competition/get", {
         headers: { Authorization: `Bearer ${token}` },
@@ -58,6 +63,79 @@ class Events extends Component {
     return (
       <React.Fragment>
         <div className="col-11 mx-auto mt-5">
+          <div className="row col-12 mx-auto">
+            {" "}
+            <Section
+              img="https://image.flaticon.com/icons/svg/3003/3003252.svg"
+              name="Webinars"
+              className={classes.webinars + " col-12 col-lg-6 mb-4 mb-lg-3"}
+            >
+              {this.state.webinars.length === 0 ? (
+                <Event
+                  name="Coming Soon"
+                  description="Stay tuned!"
+                  image={aaruush}
+                />
+              ) : (
+                this.state.webinars.map((webinar) => (
+                  <React.Fragment>
+                    <Event
+                      key={webinar._id}
+                      name={webinar.heading}
+                      description={webinar.description}
+                      image={webinar.imageUrl}
+                      click={webinar.link}
+                    />
+                    {window.location.pathname === "/admin/panel" ? (
+                      <Button
+                        className="col-12 mb-4"
+                        color="danger"
+                        outline
+                        onClick={() => this.props.webinarRemove(webinar._id)}
+                      >
+                        Remove
+                      </Button>
+                    ) : null}
+                  </React.Fragment>
+                ))
+              )}
+            </Section>
+            <Section
+              img="https://image.flaticon.com/icons/svg/3003/3003246.svg"
+              name="Workshops"
+              className={classes.workshops + " col-12 col-lg-6 mb-4 mb-lg-3"}
+            >
+              {this.state.workshops.length === 0 ? (
+                <Event
+                  name="Coming Soon"
+                  description="Stay tuned!"
+                  image={aaruush}
+                />
+              ) : (
+                this.state.workshops.map((workshop) => (
+                  <React.Fragment>
+                    <Event
+                      key={workshop._id}
+                      name={workshop.heading}
+                      description={workshop.description}
+                      image={workshop.imageUrl}
+                      click={workshop.link}
+                    />
+                    {window.location.pathname === "/admin/panel" ? (
+                      <Button
+                        className="col-12 mb-4"
+                        color="danger"
+                        outline
+                        onClick={() => this.props.workshopRemove(workshop._id)}
+                      >
+                        Remove
+                      </Button>
+                    ) : null}
+                  </React.Fragment>
+                ))
+              )}
+            </Section>
+          </div>
           <div className="row col-12 mx-auto">
             <Section
               img="https://image.flaticon.com/icons/svg/3022/3022689.svg"
@@ -125,78 +203,6 @@ class Events extends Component {
                         onClick={() =>
                           this.props.certificateRemove(certificate._id)
                         }
-                      >
-                        Remove
-                      </Button>
-                    ) : null}
-                  </React.Fragment>
-                ))
-              )}
-            </Section>
-          </div>
-          <div className="row col-12 mx-auto">
-            <Section
-              img="https://image.flaticon.com/icons/svg/3003/3003252.svg"
-              name="Webinars"
-              className={classes.webinars + " col-12 col-lg-6 mb-4 mb-lg-3"}
-            >
-              {this.state.webinars.length === 0 ? (
-                <Event
-                  name="Coming Soon"
-                  description="Stay tuned!"
-                  image={aaruush}
-                />
-              ) : (
-                this.state.webinars.map((webinar) => (
-                  <React.Fragment>
-                    <Event
-                      key={webinar._id}
-                      name={webinar.heading}
-                      description={webinar.description}
-                      image={webinar.imageUrl}
-                      click={webinar.link}
-                    />
-                    {window.location.pathname === "/admin/panel" ? (
-                      <Button
-                        className="col-12 mb-4"
-                        color="danger"
-                        outline
-                        onClick={() => this.props.webinarRemove(webinar._id)}
-                      >
-                        Remove
-                      </Button>
-                    ) : null}
-                  </React.Fragment>
-                ))
-              )}
-            </Section>
-            <Section
-              img="https://image.flaticon.com/icons/svg/3003/3003246.svg"
-              name="Workshops"
-              className={classes.workshops + " col-12 col-lg-6 mb-4 mb-lg-3"}
-            >
-              {this.state.workshops.length === 0 ? (
-                <Event
-                  name="Coming Soon"
-                  description="Stay tuned!"
-                  image={aaruush}
-                />
-              ) : (
-                this.state.workshops.map((workshop) => (
-                  <React.Fragment>
-                    <Event
-                      key={workshop._id}
-                      name={workshop.heading}
-                      description={workshop.description}
-                      image={workshop.imageUrl}
-                      click={workshop.link}
-                    />
-                    {window.location.pathname === "/admin/panel" ? (
-                      <Button
-                        className="col-12 mb-4"
-                        color="danger"
-                        outline
-                        onClick={() => this.props.workshopRemove(workshop._id)}
                       >
                         Remove
                       </Button>
